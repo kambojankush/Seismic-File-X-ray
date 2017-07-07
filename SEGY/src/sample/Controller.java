1c537445a6d6494d53bafcb562a9f0235f9f1429
Shoeb96 <shoeb.khan.15cse@bml.edu.in>
2017-07-07 22:02:40 +0530

    Update Controller.java

--- SEGY/src/sample/Controller.java
+++ SEGY/src/sample/Controller.java
@@ -8,15 +8,16 @@ import java.nio.Buffer;
 
 public class Controller {
 
-
-    public FileReader reader;
-    public TextArea textHeader;
+ 
+    public FileReader reader; 
+    public TextArea textHeader;  //Defining required FXML objects here
     public TextArea binaryHeader;
     public TextArea traceHeader;
     public TextArea EXTtrace;
 
-    public String opener() {
-        FileChooser fc = new FileChooser();
+    public String opener()   
+    {
+        FileChooser fc = new FileChooser();   //Filechooser class used to open select file screen
         File selectFile = fc.showOpenDialog(null);
         String path = null;
         if (selectFile != null) {
@@ -29,13 +30,14 @@ public class Controller {
         return path;
     }
 
-    public void execute() {
-        String path = "D:\\Seismic File X-ray\\src\\ReadHeaders.exe";
+    public void execute()    //Executs cpp exe file and calls opener function to get the address of Segy file, passes file names as arguments
+    { 
+        String path = "PATH OF CPP EXE FILE";   //Add. of exe file
         if (new File(path).exists())
 
         {
             try {
-                ProcessBuilder pb = new ProcessBuilder(path, opener(), "outputfile1.txt", "outputfile2.txt", "extendedtextheader.txt", "traceheader.csv", "tracedata.csv");
+                ProcessBuilder pb = new ProcessBuilder(path, opener(), "outputfile1.txt", "outputfile2.txt", "extendedtextheader.txt", "traceheader.csv", "tracedata.csv");//Passing file names, address of segyFile to it.
                 pb.redirectError();
                 Process p = pb.start();
                 InputStream is = p.getInputStream();
@@ -44,7 +46,7 @@ public class Controller {
                     System.out.print((char) value);
                 }
                 int exitCode = p.waitFor();
-                System.out.println(path + " EXITED WITH " + exitCode);
+                System.out.println(path + " EXITED WITH " + exitCode); //Printing exit code for this process
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (InterruptedException e) {
@@ -55,7 +57,8 @@ public class Controller {
 
     }
 
-    public void newFile() {
+    public void newFile() //Deletes the txt, csv files and clears the text on textarea 
+    {
         File reader = new File("C:\\Users\\Student\\IdeaProjects\\SEGY\\outputfile1.txt");
         reader.delete();
         reader = new File("C:\\Users\\Student\\IdeaProjects\\SEGY\\outputfile2.txt");
@@ -69,14 +72,14 @@ public class Controller {
         binaryHeader.clear();
         EXTtrace.clear();
 
-        execute();
+        execute();  // Calling execute function again to open the Select-file screen and run the cpp exe file again
     }
 
-    public void readfile(){
+    public void readfile() // Read function to read the files and displaying them on Text Area 
+    {
         BufferedReader br;
-
         try {
-            FileReader reader = new FileReader("C:\\Users\\Student\\IdeaProjects\\SEGY\\outputfile1.txt");
+            FileReader reader = new FileReader("Path of text header file");
             br = new BufferedReader(reader);
             String line, newLine = "\n";
 
@@ -85,7 +88,7 @@ public class Controller {
                 textHeader.appendText(newLine);
 
             }
-            reader = new FileReader("C:\\Users\\Student\\IdeaProjects\\SEGY\\outputfile2.txt");
+            reader = new FileReader("Path of Binary Header file");
             br = new BufferedReader(reader);
 
 
@@ -94,16 +97,14 @@ public class Controller {
                 binaryHeader.appendText(newLine);
 
             }
-            reader = new FileReader("C:\\Users\\Student\\IdeaProjects\\SEGY\\traceheader.csv");
+            reader = new FileReader("Path of trace header file");
             br = new BufferedReader(reader);
-
-
 
             traceHeader.appendText( br.readLine().replaceAll(",","\n"));
             traceHeader.appendText(newLine);
 
 
-            reader = new FileReader("C:\\Users\\Student\\IdeaProjects\\SEGY\\extendedtextheader.txt");
+            reader = new FileReader("Path of Extended Trace Header file");
             br = new BufferedReader(reader);
 
             while ((line = br.readLine()) != null) {
