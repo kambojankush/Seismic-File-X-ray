d314f40ae3f9d75180882e53fdbc3c1981baad9c
Ankush Kamboj <ankushkamboj06@gmail.com>
2017-07-17 14:36:10 +0530

    Added python file to display a particular trace

---
+++ src/plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import csv
+
+with open('../output/TraceData.csv', 'rt') as f:
+	reader = csv.reader(f)
+	traces = list(reader)
+	time = list(range(2500))
+
+for samples in traces:
+	samples = [float(i) for i in samples]
+	with plt.style.context('seaborn-poster'):
+		plt.plot(time, samples, linewidth = 0.1,color="red", label = 'x')
+
+plt.xlabel('Time  (μs)')
+plt.ylabel('Data Sample')
+plt.tick_params(top='off', bottom='off', left='off', right='off', labelleft='on', labelbottom='on')
+fig = plt.gcf()
+
+fig.savefig('../output/tracedata.png') 
+
+	
+
